02f1f55e879b00362dbcdc1b6b4dfb39ed0c49e1
Pranay Jadhao <pranayvjadhao197@gmail.com>
2026-06-05 22:57:52 +0530

    Update excp.py

--- excp.py
+++ excp.py
@@ -3,7 +3,6 @@ print("Program starts...")
 try:
     a = int(input("Enter first number : "))
     b = int(input("Enter second number : "))
-
     c = a // b   # Integer division like Java
 
     print("The division of", a, "and", b, "is", c)
